f3c4613dfbed275aa251205d829e8c5b6039b447
Garvit Bhardwaj <38141387+GARVITBHARDWAJ@users.noreply.github.com>
2021-01-01 13:53:40 +0530

    comments included

--- CRD_Operations/CRD.py
+++ CRD_Operations/CRD.py
@@ -11,13 +11,13 @@ d={}
 l=[]
 app = Flask(__name__)
 
-@app.route("/", methods = ["POST"])
+@app.route("/", methods = ["POST"])                                 
 def main():
     reqBody = request.get_json()
     input = reqBody['input']
     while(1):
         print("Select the operation you want to do:\n")
-        print("1-Create\n2-Read\n3-Delete\n4-Show all\n5-Exit\n")
+        print("1-Create\n2-Read\n3-Delete\n4-Show all\n5-Exit\n")   #choose number to perform specific operation
         opt=input
         if(opt==1):
             c = create()
@@ -30,15 +30,15 @@ def main():
             return(d)
         else:
             print("Invalid Operation\n")
-    t1=Thread(target=(create or read or delete)) 
+    t1=Thread(target=(create or read or delete))                    # THREAD added
     t1.start()
     time.sleep(1)
     t2=Thread(target=(create or read or delete))
     t2.start()
     time.sleep(1)
 
-    f=open("garvit.txt",'w')
-    strc=str(d)
+    f=open("garvit.txt",'w')                                         #Storing the data in given file.
+    strc=str(d)                                                      #User can also provide the path for specific file.           
     f.write(strc)
     f=open("garvit.txt",'r')
     print(f.read())
@@ -49,11 +49,11 @@ def main():
 
 @app.route("/create",methods = ["POST"])
 def create():
-    reqBody = request.get_json()    #get response from json
-    key = reqBody['key']            #get Key from json  
-    value = reqBody['value']        #get value from json
-    ti=reqBody['time']              #get time in minutes from json
-    ti=ti*60                        #converting time from minutes to seconds
+    reqBody = request.get_json()                                    #get response from json
+    key = reqBody['key']                                            #get Key from json  
+    value = reqBody['value']                                        #get value from json
+    ti=reqBody['time']                                              #get time in minutes from json
+    ti=ti*60                                                        #converting time from minutes to seconds
     if key in d:
         return("Error: this key is Already exists:\n")
     else:
@@ -70,10 +70,10 @@ def create():
         else:
             return("Error: Memory Limit exceed\n") 
 
-@app.route("/read",methods = ["POST"])                  #read function
+@app.route("/read",methods = ["POST"])                              #read function
 def read():
     print("Enter key to read the pair\n")
-    reqBody = request.get_json()                        #getting input from json file. 
+    reqBody = request.get_json()                                    #getting input from json file. 
     key = reqBody['key']
     res = {}
     if key not in d:
@@ -90,10 +90,10 @@ def read():
             res[key] = b[0]
             return(res)
 
-@app.route("/delete",methods = ["POST"])                #delete function.
+@app.route("/delete",methods = ["POST"])                            #delete function.
 def delete():
     print("Enter the key to delete\n")
-    reqBody = request.get_json()                        #getting input from json file.
+    reqBody = request.get_json()                                    #getting input from json file.
     key = reqBody['key']
     if key not in d:
         return("Error: Key does not Exist\n")
